665f3124954ecd2869cd37babbd120b6e76b5c2b
AlexDL42 <adl42.pro@gmail.com>
2023-04-23 21:57:49 +0200

    rfe

--- pages/liste.tsx
+++ pages/liste.tsx
@@ -16,7 +16,7 @@ export default function WeddingListPage () {
                     <Header/>
                     <div className="page-cover-buffer"/>
                     <div id="anchor_one" />
-                    <div className="page-content-bg p-20">
+                    <div className="page-content-bg py-20">
                         <div className="page-content-title">
                             Si vous voulez participer ...
                         </div>
