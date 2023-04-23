d2482e77f38e81da8e5fa7c2ed9313fcea0f6198
AlexDL42 <adl42.pro@gmail.com>
2023-04-23 22:53:25 +0200

    rtfe

--- components/rsvp-form.tsx
+++ components/rsvp-form.tsx
@@ -124,7 +124,7 @@ function Example() {
                             type="email"
                             id="email"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 drk:bg-gray-700 drk:border-gray-600 drk:placeholder-gray-400 drk:text-white drk:focus:ring-blue-500 drk:focus:border-blue-500"
-                            placeholder="john.doe@company.com"
+                            placeholder="john.doe@gmail.com"
                             required
                         />
                     </div>
