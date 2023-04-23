021da37a6346fd884816617c3d14ac024533ecba
AlexDL42 <adl42.pro@gmail.com>
2023-04-23 20:42:24 +0200

    fixed navbar

--- components/header.tsx
+++ components/header.tsx
@@ -13,6 +13,12 @@ const LinkCustom = ({ href, children }) => {
 }
 
 const Header = () => {
+
+  const handleClick = () => {
+    document.getElementById('nav-toggle').onclick = function(){
+			document.getElementById("nav-content").classList.toggle("hidden");
+		}
+  }
   return (
     <div className="header-container">
       <div className="header-home-container">
@@ -20,8 +26,18 @@ const Header = () => {
           Home
         </LinkCustom>
       </div>
-
-      <div className="header-nav-container">
+      <div className="block lg:hidden">
+        <button
+          id="nav-toggle"
+          onClick={function(){
+            document.getElementById("nav-content").classList.toggle("hidden");
+          }}
+          className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-green-800 hover:border-green-800"
+        >
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div className="header-nav-container hidden" id="nav-content">
 
         <LinkCustom href="/planning/#anchor_one">
           Planning
